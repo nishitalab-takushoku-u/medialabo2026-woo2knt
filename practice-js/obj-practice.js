36215e6ae720a55bf78e31d48248f9784ec9bf92
Kanata Utsu <258424@st.takushoku-u.ac.jp>
2026-06-18 16:06:12 +0900

    js追加

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -16,3 +16,10 @@ let gakka = [
 
 //////////////// ここから下にプログラムを書きたそう!
 
+console.log(campus.address);
+for(let a of campus.buildingD){
+	console.log(a);
+}
+for(let a of gakka){
+	console.log(a.name);
+}
